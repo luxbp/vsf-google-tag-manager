1a918d6f28167cd18d940a3b535b3b5bbda27e95
Brian McDonald <bmcdonald@luxbp.com>
2020-03-03 15:03:04 -0500

    feat(route-change): clean route data

--- events/RouteChange.ts
+++ events/RouteChange.ts
@@ -5,15 +5,23 @@ import {Route} from 'vue-router'
 declare const dataLayer
 
 export default (to: Route, from: Route): void => {
+
+    let keys = ['name', 'path', 'fullPath', 'query', 'params', 'meta']
+    let pick = (obj, keys) => {
+        return keys.map(k => k in obj ? {[k]: obj[k]} : {})
+            .reduce((res, o) => Object.assign(res, o), {});
+    }
+
     try {
         if (!('dataLayer' in window)) {
             throw new Error("GTM not installed")
         }
+
         dataLayer.push({
-            'event': 'virtualPageView',
-            'page': {
-                to: {...to},
-                from: {...from}
+            event: 'virtualPageView',
+            page: {
+                to: pick(to, keys),
+                from: pick(from, keys)
             }
         });
     } catch (e) {

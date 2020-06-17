fb288acb50fcd550d3793202f2d388a8fdd24d83
Brian McDonald <bmcdonald@luxbp.com>
2020-06-17 11:54:51 -0400

    product use recently viewed mutation

--- subscribers/ProductDetailsSubscriber.ts
+++ subscribers/ProductDetailsSubscriber.ts
@@ -1,5 +1,4 @@
 import createProductData from '../helper/createProductData';
-import {CATALOG_SET_PRODUCT_CURRENT} from '@vue-storefront/core/modules/catalog/store/product/mutation-types';
 import rootStore from '@vue-storefront/core/store'
 import {KEY} from '../index';
 
@@ -8,12 +7,12 @@ declare const dataLayer;
 export default (store) => store.subscribe((mutation, state) => {
   const type = mutation.type;
 
-  if (type.endsWith(CATALOG_SET_PRODUCT_CURRENT)) {
+  if (type.endsWith('recently-viewed/recently-viewed/ADD')) {
     dataLayer.push({
       'ecommerce': {
         'detail': {
           'actionField': {'list': rootStore.state[KEY].source || null},
-          'products': [createProductData(mutation.payload)]
+          'products': [createProductData(mutation.payload.product)]
         }
       }
     });
